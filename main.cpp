b82a126e0db47e9a65f62f85fae2d768efba2c16
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-09-08 23:35:10 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Michael Coleman
+Course: CSCI-135
+Instructor:  Genady Maryash
+Assignment: Lab 2b
+This program prints out all the integers in the range of L <= i < U.
+*/
+
+#include <iostream>
+#include "funcs.h"
+
+int main() {
+ std::cout << print_interval(-5, 10);
+
+}
